a26e935fb586999f7b98d7b4d5d7fb9dc8e58f55
ÍõÑǴ³ <1975670198@qq.com>
2026-03-26 16:39:28 +0800

    update

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = '王亚闯的FPGA博客111111111';
-export const SITE_DESCRIPTION = '专注 FPGA / Verilog / 时序优化 / 高速接口开发!';
+export const SITE_DESCRIPTION = '专注 FPGA / Verilog / 时序优化 / 高速接口开发!   高速接口开发 高速接口开发 高速接口开发 高速接口开发 高速接口开发 高速接口开发';
